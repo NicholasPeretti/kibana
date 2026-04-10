bc47683795744aa7250fb7cf9e038e15ddbcbf66
Nicholas Peretti <nicholas.peretti@elastic.co>
2026-04-10 14:36:14 +0100

    Refactor test according to PR feedback

--- x-pack/solutions/security/plugins/security_solution/test/scout_security_attacks_alignment/ui/parallel_tests/detections/attacks/attack_flyout.spec.ts
+++ x-pack/solutions/security/plugins/security_solution/test/scout_security_attacks_alignment/ui/parallel_tests/detections/attacks/attack_flyout.spec.ts
@@ -20,47 +20,52 @@ spaceTest.describe(
       await apiServices.attackDiscovery.seedAttackSchedule();
     });
 
-    spaceTest.beforeEach(async ({ browserAuth, scoutSpace }) => {
+    spaceTest.beforeEach(async ({ browserAuth, scoutSpace, pageObjects }) => {
+      const { detectionsAttackDiscoveryPage, attackDetailsRightPanelPage } = pageObjects;
+
       await scoutSpace.uiSettings.set({
         [ENABLE_ALERTS_AND_ATTACKS_ALIGNMENT_SETTING]: true,
       });
       await browserAuth.loginAsAdmin();
+
+      await detectionsAttackDiscoveryPage.navigateToAttacksPage();
+      await expect(detectionsAttackDiscoveryPage.attacksTableSection).toBeAttached();
+      await detectionsAttackDiscoveryPage.attacksTableSection.scrollIntoViewIfNeeded();
+      await expect(detectionsAttackDiscoveryPage.attacksTableSection).toBeVisible();
+      await expect(detectionsAttackDiscoveryPage.tableExpandAttackDetailsButtons).toHaveCount(1);
+      await expect(detectionsAttackDiscoveryPage.tableExpandAttackDetailsButtons).toBeVisible();
+      await detectionsAttackDiscoveryPage.openFirstAttackDetailsFromTable();
+      await expect(attackDetailsRightPanelPage.detailsFlyoutBody).toBeAttached();
+      await attackDetailsRightPanelPage.detailsFlyoutBody.scrollIntoViewIfNeeded();
+      await expect(attackDetailsRightPanelPage.detailsFlyoutBody).toBeVisible();
     });
 
     spaceTest.afterEach(async ({ scoutSpace }) => {
       await scoutSpace.uiSettings.unset(ENABLE_ALERTS_AND_ATTACKS_ALIGNMENT_SETTING);
     });
 
-    spaceTest(
-      'shows Insights and Correlations in the attack details flyout',
-      async ({ pageObjects }) => {
-        const { detectionsAttackDiscoveryPage, attackDetailsRightPanelPage } = pageObjects;
+    spaceTest('shows Insights section in attack details flyout', async ({ pageObjects }) => {
+      const { attackDetailsRightPanelPage } = pageObjects;
 
-        await detectionsAttackDiscoveryPage.navigateToAttacksPage();
-        await expect(detectionsAttackDiscoveryPage.attacksTableSection).toBeAttached();
-        await detectionsAttackDiscoveryPage.attacksTableSection.scrollIntoViewIfNeeded();
-        await expect(detectionsAttackDiscoveryPage.attacksTableSection).toBeVisible();
-        await expect(detectionsAttackDiscoveryPage.tableExpandAttackDetailsButtons).toHaveCount(1);
-        await expect(detectionsAttackDiscoveryPage.tableExpandAttackDetailsButtons).toBeVisible();
+      await expect(attackDetailsRightPanelPage.insightsSectionHeader).toBeAttached();
+      await attackDetailsRightPanelPage.insightsSectionHeader.scrollIntoViewIfNeeded();
+      await expect(attackDetailsRightPanelPage.insightsSectionHeader).toBeVisible();
+      await attackDetailsRightPanelPage.expandInsightsSectionIfCollapsed();
+      await expect(attackDetailsRightPanelPage.insightsSectionContent).toBeAttached();
+      await attackDetailsRightPanelPage.insightsSectionContent.scrollIntoViewIfNeeded();
+      await expect(attackDetailsRightPanelPage.insightsSectionContent).toBeVisible();
+    });
 
-        await detectionsAttackDiscoveryPage.openFirstAttackDetailsFromTable();
+    spaceTest('shows Correlations section in attack details flyout', async ({ pageObjects }) => {
+      const { attackDetailsRightPanelPage } = pageObjects;
 
-        await expect(attackDetailsRightPanelPage.detailsFlyoutBody).toBeAttached();
-        await attackDetailsRightPanelPage.detailsFlyoutBody.scrollIntoViewIfNeeded();
-        await expect(attackDetailsRightPanelPage.detailsFlyoutBody).toBeVisible();
-
-        await expect(attackDetailsRightPanelPage.insightsSectionHeader).toBeAttached();
-        await attackDetailsRightPanelPage.insightsSectionHeader.scrollIntoViewIfNeeded();
-        await expect(attackDetailsRightPanelPage.insightsSectionHeader).toBeVisible();
-        await attackDetailsRightPanelPage.expandInsightsSectionIfCollapsed();
-        await expect(attackDetailsRightPanelPage.insightsSectionContent).toBeAttached();
-        await attackDetailsRightPanelPage.insightsSectionContent.scrollIntoViewIfNeeded();
-        await expect(attackDetailsRightPanelPage.insightsSectionContent).toBeVisible();
-
-        await expect(attackDetailsRightPanelPage.correlationsSection).toBeAttached();
-        await attackDetailsRightPanelPage.correlationsSection.scrollIntoViewIfNeeded();
-        await expect(attackDetailsRightPanelPage.correlationsSection).toBeVisible();
-      }
-    );
+      await attackDetailsRightPanelPage.expandInsightsSectionIfCollapsed();
+      await expect(attackDetailsRightPanelPage.insightsSectionContent).toBeAttached();
+      await attackDetailsRightPanelPage.insightsSectionContent.scrollIntoViewIfNeeded();
+      await expect(attackDetailsRightPanelPage.insightsSectionContent).toBeVisible();
+      await expect(attackDetailsRightPanelPage.correlationsSection).toBeAttached();
+      await attackDetailsRightPanelPage.correlationsSection.scrollIntoViewIfNeeded();
+      await expect(attackDetailsRightPanelPage.correlationsSection).toBeVisible();
+    });
   }
 );
